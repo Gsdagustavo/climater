82d5f5c76706c9bc3194ab03f554c67a0dfaca90
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-08 08:03:50 -0300

    refactor: move the homepage to a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:climater/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 const String appTitle = 'Climater';
@@ -21,19 +22,6 @@ class MyApp extends StatelessWidget {
       },
 
       initialRoute: '/homePage',
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
-      ),
     );
   }
 }
