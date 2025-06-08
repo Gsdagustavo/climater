1039c81f0ddec6df581bd5629a07b51b7513ddef
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-08 14:01:50 -0300

    feat: implement the theme provider in the MyApp component

--- lib/components/my_app.dart
+++ lib/components/my_app.dart
@@ -1,4 +1,6 @@
+import 'package:climater/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../pages/home_page.dart';
 
@@ -10,15 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appTitle,
-      debugShowCheckedModeBanner: showDebugBanner,
-
-      routes: {
-        '/homePage': (_) => const HomePage(),
-      },
-
-      initialRoute: '/homePage',
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (_, state, __) {
+          return MaterialApp(
+            title: appTitle,
+            debugShowCheckedModeBanner: showDebugBanner,
+            routes: {'/homePage': (_) => const HomePage()},
+            initialRoute: '/homePage',
+            theme: state.isDarkMode ? darkThemeData : lightThemeData,
+          );
+        },
+      ),
     );
   }
 }
