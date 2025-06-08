db0aaf6abc35239f4565cd5ca3fe43a4d4d11d54
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-08 16:15:37 -0300

    feat: add the main and description text for the weather in the WeatherData model

--- lib/model/weather_data.dart
+++ lib/model/weather_data.dart
@@ -6,6 +6,8 @@ class WeatherData {
   final double feelsLike;
   final int pressure;
   final int humidity;
+  final String main;
+  final String description;
 
   WeatherData({
     required this.address,
@@ -13,5 +15,17 @@ class WeatherData {
     required this.feelsLike,
     required this.pressure,
     required this.humidity,
+    required this.main,
+    required this.description,
   });
+
+  String formatDescription() {
+    return description
+        .split(' ')
+        .map((word) {
+          if (word.isEmpty) return '';
+          return word[0].toUpperCase() + word.substring(0, word.length);
+        })
+        .join(' ');
+  }
 }
