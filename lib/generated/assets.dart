a0eab6b1da2c8ee16330268987b617f48f23a4a3
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-08 17:20:13 -0300

    chore: move assets/images/screenshots to assets/images

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -21,9 +21,9 @@ class Assets {
   static const String images13n2x = 'assets/images/13n@2x.png';
   static const String images50d2x = 'assets/images/50d@2x.png';
   static const String images50n2x = 'assets/images/50n@2x.png';
-  static const String resultsClimaterDrawerDarkMode = 'assets/images/results/drawer_dark.png';
-  static const String resultsClimaterDrawerLightMode = 'assets/images/results/drawer_light.png';
-  static const String resultsClimaterHpDarkMode = 'assets/images/results/home_dark.png';
-  static const String resultsClimaterHpLightMode = 'assets/images/results/home_light.png';
+  static const String screenshotsDrawerDark = 'assets/screenshots/drawer_dark.png';
+  static const String screenshotsDrawerLight = 'assets/screenshots/drawer_light.png';
+  static const String screenshotsHomeDark = 'assets/screenshots/home_dark.png';
+  static const String screenshotsHomeLight = 'assets/screenshots/home_light.png';
 
 }
