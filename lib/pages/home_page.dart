e1f9586e59bad92ced332d28baaca597af43e278
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-08 10:47:41 -0300

    feat: implement the weather provider in the homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,37 @@
+import 'package:climater/providers/weather_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (_) => WeatherProvider(),
+      child: Scaffold(
+        body: Center(
+          child: Consumer<WeatherProvider>(
+            builder: (_, state, __) {
+              return FutureBuilder(
+                future: state.getWeatherData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+
+                  final String? data = snapshot.data;
+
+                  if (data == null) {
+                    return Text(snapshot.error.toString());
+                  }
+
+                  return Text(snapshot.data!);
+                },
+              );
+            },
+          ),
+        ),
       ),
     );
   }
