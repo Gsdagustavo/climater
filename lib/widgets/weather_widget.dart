3779e80cfec2e1d977740eb1793bcd1a7ee01358
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-10 21:22:58 -0300

    style: add main labels to the infos

--- lib/widgets/weather_widget.dart
+++ lib/widgets/weather_widget.dart
@@ -44,106 +44,136 @@ class WeatherWidget extends StatelessWidget {
 
         Padding(padding: EdgeInsets.all(32)),
 
-        /// Min and Max temperatures and feels like
-        SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            spacing: 15,
-            children: [
-              FabContainer(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(FontAwesomeIcons.temperatureLow),
-
-                    Padding(padding: EdgeInsets.all(5)),
-
-                    Text(
-                      'Min: ${weatherData.minTemp.toStringAsFixed(0)} °C',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                  ],
-                ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Text(
+                'Temperature',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
+            ),
 
-              FabContainer(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(FontAwesomeIcons.temperatureHigh),
+            Padding(padding: EdgeInsets.all(8)),
 
-                    Padding(padding: EdgeInsets.all(5)),
+            /// Min and Max temperatures and feels like
+            SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                spacing: 15,
+                children: [
+                  FabContainer(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(FontAwesomeIcons.temperatureLow),
 
-                    Text(
-                      'Max: ${weatherData.maxTemp.toStringAsFixed(0)} °C',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
+                        Padding(padding: EdgeInsets.all(5)),
+
+                        Text(
+                          'Min: ${weatherData.minTemp.toStringAsFixed(0)} °C',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
+                  ),
 
-              FabContainer(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(FontAwesomeIcons.temperatureHalf),
+                  FabContainer(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(FontAwesomeIcons.temperatureHigh),
 
-                    Padding(padding: EdgeInsets.all(5)),
+                        Padding(padding: EdgeInsets.all(5)),
 
-                    Text(
-                      'Feels like: ${weatherData.feelsLike.toStringAsFixed(0)} °C',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
+                        Text(
+                          'Max: ${weatherData.maxTemp.toStringAsFixed(0)} °C',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+
+                  FabContainer(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(FontAwesomeIcons.temperatureHalf),
+
+                        Padding(padding: EdgeInsets.all(5)),
+
+                        Text(
+                          'Feels like: ${weatherData.feelsLike.toStringAsFixed(0)} °C',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
 
         Padding(padding: EdgeInsets.all(12)),
 
-        /// Humidity and Pressure
-        SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              FabContainer(
-                child: Row(
-                  children: [
-                    Icon(Icons.water_drop_outlined),
-
-                    Padding(padding: EdgeInsets.all(5)),
-
-                    Text(
-                      'Humidity: ${weatherData.humidity}%',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                  ],
-                ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Text(
+                'Other infos',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
+            ),
 
-              Padding(padding: EdgeInsets.all(12)),
+            Padding(padding: EdgeInsets.all(8)),
 
-              FabContainer(
-                child: Row(
-                  children: [
-                    Icon(FontAwesomeIcons.gaugeHigh),
+            /// Humidity and Pressure
+            SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  FabContainer(
+                    child: Row(
+                      children: [
+                        Icon(Icons.water_drop_outlined),
 
-                    Padding(padding: EdgeInsets.all(5)),
+                        Padding(padding: EdgeInsets.all(5)),
 
-                    Text(
-                      'Pressure: ${weatherData.pressure} hpa',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+                        Text(
+                          'Humidity: ${weatherData.humidity}%',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+
+                  Padding(padding: EdgeInsets.all(12)),
+
+                  FabContainer(
+                    child: Row(
+                      children: [
+                        Icon(FontAwesomeIcons.gaugeHigh),
+
+                        Padding(padding: EdgeInsets.all(5)),
+
+                        Text(
+                          'Pressure: ${weatherData.pressure} hpa',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ],
     );
